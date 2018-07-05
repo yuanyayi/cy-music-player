d8fee155d6bf6884ced7e038930528f5743651a0
YuanYayi <callmecrystal@163.com>
2018-07-05 11:30:32 +0800

    更新独立进度条组件，更新callback方式

--- static/js/cyPlayer.js
+++ static/js/cyPlayer.js
@@ -1,9 +1,9 @@
-let songsList = [ {
+let songsListJson = [{
   songName: 'You Are My Everything',
   singerName: 'Lexington Bridge',
   src: 'You Are My Everything.m4a',
   img: 'lexington-bridge-15.jpeg',
-},{
+}, {
   songName: 'Wildest Dreams',
   singerName: 'Taylor Swift',
   src: 'Wildest Dreams.m4a',
@@ -26,181 +26,192 @@ let songsList = [ {
 }]
 
 function cyPlayer(domEl) {
-  let $el = $(domEl)
-  let _this = this
-  // 设置实体参数
-  this.songsList = songsList
-  // ------------------- create DOMs --------------------- //
-  this.player = $el.find('audio')[0]
-  // info
-  this.infoPanel = $el.find('.infoPanel')
-  // controllers
-  this.controller = $el.find('.controllerPanel')
-  // 播放Btn
-  this.playBtn = this.controller.children('i.play')
-  // 暂停Btn
-  this.pauseBtn = this.controller.children('i.pause')
-  // 切换Btn
-  this.nextBtn = this.controller.children('i.next')
-  this.prevBtn = this.controller.children('i.prev')
-  // 播放列表Btn
-  this.menuBtn = this.controller.children('i.menu')
-  // 音量控制
-  this.soundBtn = this.controller.find('i.sound')
-  this.muteBtn = this.controller.find('i.mute')
-  this.volumeBox = this.controller.children('.volumeBox')
-  // songProgress
-  this.songProgress = new ProgressBar($el.find('.progress'), (value) => {
-    _this.currentTime(Math.floor(value))
-  }, this.player.duration)
-  this.currentTimeText = $el.find('.time.current')
-  this.durationTimeText = $el.find('.time.duration')
-  // playList
-  this.playListWrap = $el.find('.playListWrap')
-  this.playList = $el.find('.playList')
-  // playList Index
-  this.songsIndexList = []
-  for (let i = 0; i < this.songsList.length; i++) {
-    this.songsIndexList.push(i)
+  let $el = $(domEl),
+    _this = this,
+    // 设置实体参数
+    songsList = songsListJson,
+    // ------------------- create DOMs --------------------- //
+    audio = $el.find('audio')[0] || $el.append('<audio preload="auto" autoplay="false" hidden"></audio>').find('audio')[0],
+    // 播放Btn
+    playBtn = $el.find('.play'),
+    // 暂停Btn
+    pauseBtn = $el.find('.pause'),
+    // 切换Btn
+    nextBtn = $el.find('.next'),
+    prevBtn = $el.find('.prev'),
+    // 播放列表Btn
+    menuBtn = $el.find('.menu'),
+    // 音量控制
+    soundBtn = $el.find('i.sound'),
+    muteBtn = $el.find('i.mute'),
+    volumeBox = $el.find('.volumeBox'),
+    // 喜欢/收藏Btn
+    heartBtn = $el.find('.heart'),
+    // 播放器信息
+    currentTime = $el.find('.current'),
+    durationTime = $el.find('.duration'),
+    // 正在播放
+    indexNow = 0,
+    // songProgress
+    songProgress = new Progress($el.find('.progressBox'), {
+      max: audio.duration || 1,
+      callback: function(val) {
+        console.log(val)
+      },
+      labels: $el.find('.current'),
+      labelFormat: formatTime,
+    }),
+    currentTimeText = $el.find('.current'),
+    durationTimeText = $el.find('.duration'),
+    // playList
+    playListWrap = $el.find('.playListWrap'),
+    playList = $el.find('.playList'),
+    // playList Index
+    songsIndexList = [];
+  for (let i = 0; i < songsList.length; i++) {
+    songsIndexList.push(i)
   }
-  // console.log(this.songsIndexList)
-  // the song for now
-  this.indexNow = 0
+
+  this.song = songProgress
 
   // ------------------- DOM行为 --------------------- //
   // 播放列表
-  this.menuBtn.on('click', () => {
-    this.playListWrap.slideToggle()
+  menuBtn.on('click', () => {
+    playListWrap.slideToggle()
   })
   // 静音
-  this.soundBtn.on('click', () => {
-    this.volumeBox.addClass('mute')
+  soundBtn.on('click', () => {
     this.savedVolume = this.volume()
     this.volume(0)
-    this.volumeProgress.moveProgressBar(0)
-    this.volumeProgress.setValue(0)
+    muteBtn.show()
+    soundBtn.hide()
   })
-  this.muteBtn.on('click', () => {
-    this.volumeBox.removeClass('mute')
-    this.volume(this.savedVolume)
-    this.volumeProgress.moveProgressBar(this.savedVolume)
-    this.volumeProgress.setValue(0)
+  muteBtn.on('click', () => {
+    this.volume(this.savedVolume || 0.5)
+    muteBtn.hide()
+    soundBtn.show()
   })
   // ------------------- 基本方法 --------------------- //
-  this.player.onloadedmetadata = () => {
-    this.currentTimeText.text(formatTime(0))
-    this.durationTimeText.text(formatTime(this.player.duration))
-    this.songProgress.changeMax(this.player.duration)
+  audio.onloadedmetadata = () => {
+    currentTimeText.text(formatTime(0))
+    durationTimeText.text(formatTime(audio.duration))
+    songProgress.max = audio.duration
   }
   // 改变音量
   this.volume = (value) => {
-    if (value >= 0 && value <= 1) {
-      this.player.volume = value
-    }
-    return this.player.volume
+    volumeProgress.setValue(value)
+    return audio.volume
   }
   // volumeProgress
-  this.volumeProgress = new ProgressBar(this.controller.find('.volume'), (value) => {
-    _this.volume(value)
-    if (window.localStorage) {
-      localStorage.cyVolume = _this.volume()
+  volumeProgress = new Progress(volumeBox, {
+    value: (function() {
+      return window.localStorage ? localStorage.cyVolume : 0.5
+    })(),
+    callback: function(value) {
+      if (value >= 0 && value <= 1) {
+        audio.volume = value
+      }
+      if (window.localStorage) {
+        localStorage.cyVolume = audio.volume
+      }
     }
   })
   // volume init
   let initVolume = window.localStorage && localStorage.cyVolume ? localStorage.cyVolume : 0.5
-  this.volumeProgress.setProgressValue(initVolume)
+  volumeProgress.setValue(initVolume)
   // 媒体进度
   this.currentTime = (value) => {
     if (typeof value === 'number') {
       // 输入一个小数：
       if (value > 0 && value < 1) {
-        this.player.currentTime = Math.floor(this.player.duration * value)
+        audio.currentTime = Math.floor(audio.duration * value)
       }
       // 输入一个整数：
-      else if (value <= this.player.duration) {
-        this.player.currentTime = Math.floor(value)
+      else if (value <= audio.duration) {
+        audio.currentTime = Math.floor(value)
       }
     }
-    return this.player.currentTime
+    return audio.currentTime
   }
   // 进度条计时器
-  this._playTimer = {
+  _playTimer = {
     playingTimer: { a: 1 },
     start: () => {
       let _this = this
       // 改变进度条
-      function todo() {
-        let d = _this.player.duration
-        _this.songProgress._setProgressValue(_this.player.currentTime + 1)
-        _this.currentTimeText.text(formatTime(_this.player.currentTime))
+      function run() {
+        songProgress.setValue(audio.currentTime, false)
       }
-      todo()
-      this._playTimer.playingTimer = setInterval(todo, 1000)
+      _playTimer.playingTimer = setInterval(run, 1000)
+      run()
     },
     clear: () => {
-      clearInterval(this._playTimer.playingTimer)
+      clearInterval(_playTimer.playingTimer)
     }
   }
   // 播放
-  this._play = () => {
-    this.player.play()
-  }
-  this.player.onplay = () => {
-    if (this.playBtn.is(':visible')) {
-      this.playBtn.hide()
-      this.pauseBtn.show()
+  _play = () => {
+    audio.play()
+    if (playBtn.is(':visible')) {
+      playBtn.hide()
+      pauseBtn.show()
     }
-    this._playTimer.start()
+    _playTimer.start()
   }
-  this.playBtn.on('click', this._play)
+  playBtn.on('click', _play)
   // 暂停
-  this._pause = () => {
-    this.player.pause()
-  }
-  this.player.onpause = () => {
-    if (this.playBtn.is(':hidden')) {
-      this.pauseBtn.hide()
-      this.playBtn.show()
+  _pause = () => {
+    audio.pause()
+    if (playBtn.is(':hidden')) {
+      pauseBtn.hide()
+      playBtn.show()
     }
-    this._playTimer.clear()
+    _playTimer.clear()
   }
-  this.pauseBtn.on('click', this._pause)
+  pauseBtn.on('click', _pause)
   // 停止
-  this._stop = () => {
-    this.player.pause();
-    this.player.currentTime = 0;
-    this.songProgress.setProgressValue(0), false
+  _stop = () => {
+    _pause();
+    audio.currentTime = 0;
+    songProgress.setValue(0)
   }
   // 上一下一
-  this._prev = () => {
-    this.indexNow = this.indexNow - 1 >= 0 ? this.indexNow - 1 : this.songsList.length - 1
-    this._changeSong()
+  _prev = () => {
+    indexNow = indexNow - 1 >= 0 ? indexNow - 1 : songsList.length - 1
+    _changeSong()
   }
-  this.prevBtn.on('click', this._prev)
-  this._next = () => {
-    this.indexNow = this.indexNow + 1 < this.songsList.length ? this.indexNow + 1 : 0
-    this._changeSong()
+  prevBtn.on('click', _prev)
+  _next = () => {
+    indexNow = indexNow + 1 < songsList.length ? indexNow + 1 : 0
+    _changeSong()
   }
-  this.nextBtn.on('click', this._next)
-  this.player.onended = () => {
-    this._next()
+  nextBtn.on('click', _next)
+  audio.onended = () => {
+    _next()
+    _play()
   }
   // 切换歌曲
-  this._changeSong = (i) => {
-    if (i || i === 0) { this.indexNow = i }
-    let song = this.songsList[this.songsIndexList[this.indexNow]]
+  _changeSong = (i) => {
+    if (i || i === 0) { indexNow = i }
+    let song = songsList[indexNow]
     if (!song) return false
-    this._stop()
+    _stop()
     // info
-    this.infoPanel.find('.songImage img').attr('src', './asset/images/' + song.img)
-    this.infoPanel.find('.song').text(song.songName)
-    this.infoPanel.find('.singer').text(song.singerName)
+    $el.find('.songImage img').attr('src', './asset/images/' + song.img)
+    $el.find('.bgImg').css({ 'backgroundImage': 'url(./asset/images/' + song.img + ')' })
+    $el.find('.song').text(song.songName)
+    $el.find('.singer').text(song.singerName)
+    song.like ? heartBtn.addClass('like') : ''
     // audio
-    $(this.player).find('source').attr('src', './asset/songs/' + song.src)
-    this.player.load()
+    $(audio).find('source').attr('src', './asset/songs/' + song.src)
+    audio.load()
   }
-  this.playList.on('click', 'li', (e) => {
-    this._changeSong($(e.currentTarget).index())
+  playList.on('click', 'li', (e) => {
+    _changeSong($(e.currentTarget).index())
+  })
+  heartBtn.on('click', () => {
+    heartBtn.toggleClass('like')
+    songsList[indexNow].like = heartBtn.hasClass('like')
   })
   // ------------------- keyboard actions ------------------- //
   $('body').on('keyup', (e) => {
@@ -208,109 +219,153 @@ function cyPlayer(domEl) {
     const VOLUMEDIFF = 0.05
     // space
     if (e.keyCode === 32) {
-      this.playBtn.is(':hidden') ? this._pause() : this._play()
+      playBtn.is(':hidden') ? _pause() : _play()
     }
     // left
     if (e.keyCode === 37) {
-      this._prev()
+      _prev()
     }
     // up
     if (e.keyCode === 38) {
-      let vol = this.volume() + VOLUMEDIFF <= 1 ? this.volume() + VOLUMEDIFF : 1
-      this.volume(vol)
-      this.volumeProgress.setProgressValue(vol, false)
+      let vol = volume() + VOLUMEDIFF <= 1 ? volume() + VOLUMEDIFF : 1
+      volume(vol)
+      volumeProgress.setValue(vol, false)
     }
     // right
     if (e.keyCode === 39) {
-      this._next()
+      _next()
     }
     // down
     if (e.keyCode === 40) {
-      let vol = this.volume() - VOLUMEDIFF >= 0 ? this.volume() - VOLUMEDIFF : 0
-      this.volume(vol)
-      this.volumeProgress.setProgressValue(vol, false)
+      let vol = volume() - VOLUMEDIFF >= 0 ? volume() - VOLUMEDIFF : 0
+      volume(vol)
+      volumeProgress.setValue(vol, false)
     }
   })
   // ------------------- init ------------------- // 
-  addSongsToList(this.playList)
-  this._changeSong(0)
+  addSongsToList(playList, 'slider')
+  _changeSong(0)
   // ------------------- library ------------------- //
   // 生成播放列表
-  function addSongsToList(playList) {
+  function addSongsToList(playList, type) {
     var listStr = ''
     for (var i in songsList) {
       let song = songsList[i]
-      listStr += `<li>
-  <span class="songName">${song.songName.toUpperCase()}</span>
-  <span class="singerName">--${song.singerName}</span>
-</li>`
+      listStr += `
+    <li class="item">
+      <div class="frame">
+        <img src="./asset/images/${song.img}">
+      </div>
+      <p>${song.songName.toUpperCase()}</p>
+    </li>`
+    }
+    // 幻灯多几张
+    if (type === 'slider') {
+      for (var i = 0; i < 4; i++) {
+        let song = songsList[i]
+        listStr += `
+    <li class="item">
+      <div class="frame">
+        <img src="./asset/images/${song.img}">
+      </div>
+      <p>${song.songName.toUpperCase()}</p>
+    </li>`
+      }
     }
     playList.append(listStr)
   }
-  // 进度条类
-  function ProgressBar($target, onValueChange, max, min) {
-    // init
-    this.box = $target
-    this.bar = $target.find('.bar')
-    this.boxW = this.box.width()
-    this.min = min || 0
-    this.max = max || 1
-    this.value = Math.round(this.bar.width() / this.boxW * 100) / 100
-    this.PIECE = this.boxW / (this.max - this.min)
-    this.changeMax = (num) => {
-      if (typeof num !== 'number') return false
-      this.max = num
-      this.PIECE = this.boxW / (this.max - this.min)
-    }
-    // UI
-    this._moveProgressBar = (value, animate) => {
-      this.temp = value
-      let finalWidth = this.PIECE * (value - this.min)
-      // 动画
-      if (!animate || animate === '') {
-        this.bar.width(finalWidth + 2)
-      } else if (!!animate || animate == undefined) {
-        this.bar.stop(true, true)
-        this.bar.animate({ 'width': finalWidth + 2 }, 1000)
-      }
-      return true
-    }
-    // 设置数值
-    this._setProgressValue = (value, animate) => {
-      if (this.value === value) return false
-      value = value < this.min ? this.min : value
-      value = value > this.max ? this.max : value
-      this.oldValue = this.value
-      this.value = value
-      this._moveProgressBar(value, animate)
-      return value
-    }
-    // 外部设置：
-    this.setProgressValue = (value, animate) => {
-      this._setProgressValue(value, animate)
-      this.onValueChange(this.value, this.oldValue)
-    }
-    // 点击行为
-    this.onValueChange = onValueChange
-    this.box.on('mousedown', e => {
-      $(document).on('mousemove', e => {
-        let len = e.clientX - this.box.offset().left
-        let value = len / this.boxW * (this.max - this.min)
-        this._moveProgressBar(value, false)
-      })
-      $(document).on('mouseup', e => {
-        $(document).off('mousemove')
-        $(document).off('mouseup')
-        let len = e.clientX - this.box.offset().left
-        let value = len / this.boxW * (this.max - this.min)
-        this.setProgressValue(value, false)
-      })
-    })
-    return this
-  }
 
   function formatTime(seconds) {
-    return Math.floor(seconds / 60) + ':' + ((seconds % 60) >= 10 ? '' : '0') + Math.floor(seconds % 60)
+    return Math.floor(seconds / 60) + ':' + ((seconds % 60) >= 10 ? '' : '0') + Math.round(seconds % 60)
   }
   return this
+}
+
+function Progress(obj, ...opts) {
+  let _this = this
+  let options = {
+    value: 0,
+    max: 1,
+    min: 0,
+    labels: [],
+    labelFormat: function(val) { return val },
+    callback: null
+  }
+  // 初始化
+  this.init = function() {
+    $el = $(obj)
+    this.container = _createBar($el)
+    this.bar = this.container.find('.bar')
+    opts = opts[0] || {}
+    for (let option in options) {
+      this[option] = opts[option] || options[option]
+    }
+  }
+  this.init()
+  console.log(this)
+  // 创建UI
+  function _createBar(box) {
+    if (box.find('.progress .bar').length === 0) {
+      box.append(`<div class="progress">
+        <span class="bar"></span>
+      </div>`)
+      box.find('.progress').css({
+        'position': 'relative'
+      })
+      box.find('.bar').css({
+        'position': 'absolute',
+        'left': 0,
+        'top': 0,
+        'height': '100%',
+      })
+    }
+    return box.find('.progress')
+  }
+  // 遍历并修改标签数值
+  this.changeLabels = function() {
+    let val = this.value
+    if (this.labels instanceof jQuery) {
+      this.labels.each(function() {
+        $(this).text(_this.labelFormat(val))
+      })
+    }
+  }
+  // 直接修改数值：
+  this.setValue = function(val, call) {
+    this.value = valueFormat(val)
+    console.log(this.value)
+    this.bar.css('width', (this.value - this.min) / (this.max - this.min) * 100 + '%')
+    this.changeLabels()
+    if ((call || call === undefined) && typeof this.callback === 'function') {
+      this.callback(this.value)
+    }
+  }
+  this.setValue(this.value)
+  // 拖拽
+  this.container.on('mousedown', (ev) => {
+    this.moveBar(ev)
+    this.container.on('mousemove', (ev) => {
+      let val = this.moveBar(ev)
+
+    })
+    $(document).on('mouseup', (ev) => {
+      let val = this.moveBar(ev)
+      this.setValue(val)
+
+      this.container.off('mousemove')
+      $(document).off('mouseup')
+    })
+  })
+  // 封装移动效果
+  this.moveBar = function(ev) {
+    let len = ev.clientX - this.container.offset().left
+    let val = len / this.container.width() * (this.max - this.min)
+    this.bar.css('width', len)
+    return valueFormat(val)
+  }
+  // 精度
+  function valueFormat(val) {
+    let v = Math.round(val * 100) / 100
+    return v > _this.max ? _this.max : (v < _this.min ? _this.min : v)
+  }
 }
